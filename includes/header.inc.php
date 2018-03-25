1697d0aef735289abc72100327d7428a97adb387
Cobber <codecobber@gmail.com>
2018-03-25 20:53:06 +0100

    first upload for skeleton display

---
+++ includes/header.inc.php
@@ -0,0 +1,91 @@
+<!doctype html>
+
+<!--
+=============================================
+  Developed by Code Cobber for your enjoyment
+  http://www.codecobber.co.uk
+  Free for personal and commercial use under the CCA 4.0 license:  (see www.codecobber.co.uk/license)
+=============================================
+-->
+
+<html class="no-js" lang="en">
+
+  <head>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>title | Welcome</title>
+
+    <!-- CSS Files -->
+    
+    <link rel="stylesheet" type="text/css" href="css/push.css" />
+    <link rel="stylesheet" type="text/css" href="css/skeleton_display.css" />
+    <link rel="stylesheet" type="text/css" href="css/myCss.css" />
+    <link rel="stylesheet" type="text/css" href="css/rowData.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|EB+Garamond" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/cssFonts.css" />
+
+
+    <!-- Font Awsome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+
+
+    <!-- Javascript -->
+    
+
+    <!-- Google Analytics below -->
+    <link rel="canonical" href="http://www.yoursite.co.uk/" >
+
+
+    <!-- Facebook og settings -->
+    <meta property="og:title" content="title" />
+    <meta property="og:url" content="http://www.yoursite.co.uk" />
+    <meta property="og:image" content="http://www.yoursite.co.uk/images/logo.png" />
+    <meta property="og:type" content="company" />
+
+
+
+    <!-- twitter code below -->
+
+
+
+  </head>
+  <body>
+
+
+<!-- SideNav content -->
+
+      <div id="mySidenav" class="sidenav">
+        <ul>
+        <li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></li>
+        <li><a class="blockSec" href="#" id="saveMeM" onclick="saveMe()">Home</a></li>
+        <li><a class="blockSec" id="aboutM" href="#">About</a></li>
+        <li><a class="blockSec" id="sectorsM" href="#">Sectors</a></li>
+        <li><a class="blockSec" id="staffM" href="#">Staff</a></li>
+        <li><a class="blockSec" id="clientsM" href="#">Clients</a></li>
+        <li><a class="blockSec" id="servicesM" href="#">Services</a></li>
+        <li><a class="blockSec" id="contactM" href="#">Contact</a></li>
+        </ul>
+      </div>
+
+      <div id="main" class="hide-m">
+            <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
+      </div>
+
+
+
+<!-- ==================================================================================== -->
+
+
+        <!-- Page content -->
+
+
+
---
+++ index.php
@@ -0,0 +1,154 @@
+<?php include('includes/header.inc.php'); ?>
+<div id='start' class="container">
+
+
+    <div id="bck_1" class="bck u-full-width">
+        <div id="block_1" class="row column text-center rowNo09">
+            <h1>Changing the World Through Design</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
+            <a href="#" class="button large">Learn More</a>
+            <a href="#" class="button large hollow">Learn Less</a>
+
+        </div>
+    </div>
+    <div id="bck_2" class="bck u-full-width">
+        <div id="block_2" class="hr rowNo08">
+            <hr>
+        </div>
+    </div>
+    <div id="bck_3" class="bck">
+        <div id="block_3" class="row rowNo21">
+            <div class="l4 m4 columns">
+                <div class="callout">
+                    <p><img src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B"></p>
+                    <p class="lead">Copernican Revolution caused an uproar</p>
+                </div>
+            </div>
+            <div class="l4 m4 columns">
+                <div class="callout">
+                    <p><img src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B"></p>
+                    <p class="lead">Copernican Revolution caused an uproar</p>
+                </div>
+            </div>
+            <div class="l4 m4 columns">
+                <div class="callout">
+                    <p><img src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B"></p>
+                    <p class="lead">Copernican Revolution caused an uproar</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="bck_4" class="bck">
+        <div id="block_4" class="row rowNo06">
+
+            <div class="l4 m4 columns">
+                <h3>Photoshop</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+            </div>
+            <div class="l4 m4 columns">
+                <h3>Javascript</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+            </div>
+            <div class="l4 m4 columns">
+                <h3>Marketing</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="bck_5" class="bck">
+        <div id="block_5" class="row rowNo10">
+            <div class="l6 m6 columns">
+                <img class="thumbnail" src="http://placehold.it/750x350">
+            </div>
+            <div class="l6 m6 columns">
+                <h2>Our Agency, our selves.</h2>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in
+                    libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+            </div>
+        </div>
+    </div>
+    <div id="bck_6" class="bck">
+        <div id="block_6" class="row rowNo05">
+
+            <div class="l6 m6 columns">
+                <img class="imgPlace" src="http://placehold.it/750x350">
+            </div>
+            <div class="l6 m6 columns">
+                <h2>Our Agency, our selves.</h2>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in
+                    libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+            </div>
+
+        </div>
+    </div>
+    <div id="bck_7" class="bck">
+        <div id="block_7" class="row rowNo14">
+            <div class="l3 m3 columns">
+                <h3>Lorum</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+            </div>
+            <div class="l3 m3 columns">
+                <h3>Ipsum</h3>
+                <p>
+                    Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.
+                </p>
+            </div>
+            <div class="l3 m3 columns">
+                <h3>Dolor</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+            </div>
+            <div class="l3 m3 columns">
+                <h3>Sit Amet</h3>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+            </div>
+        </div>
+    </div>
+    <div id="bck_8" class="bck">
+        <div id="block_8" class="row rowNo20">
+            <div class="l6 m6 columns">
+                <h2>Our Agency, our selves.</h2>
+                <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra.</p>
+                <p>Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+            </div>
+
+            <div class="l6 m6 columns">
+                <img class="thumbnail" src="http://placehold.it/580x320">
+            </div>
+        </div>
+    </div>
+
+    <div id="bck_3" class="bck">
+        <div id="block_3" class="row rowNo21">
+            <div class="l4 m4 columns">
+                <div class="callout">
+                    <p><img src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B"></p>
+                    <p class="lead">Copernican Revolution caused an uproar</p>
+                </div>
+            </div>
+            <div class="l4 m4 columns">
+                <div class="callout">
+                    <p><img src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B"></p>
+                    <p class="lead">Copernican Revolution caused an uproar</p>
+                </div>
+            </div>
+            <div class="l4 m4 columns">
+                <div class="callout">
+                    <p><img src="http://placehold.it/400x170&text=Pegasi B" alt="image of a planet called Pegasi B"></p>
+                    <p class="lead">Copernican Revolution caused an uproar</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div id="bck_9" class="bck">
+    <div id="block_9" class="row rowNo19">
+        <div class="l9 m4 columns">
+            <p class="text-center">Lorem ipsum dolor sit amet, nulla cras penatibus sodales nunc vestibulum ut, maecenas suscipit, mi sem dolor leo wisi id, leo vitae eget posuere purus aliquam morbi. Vulputate fusce sapien non, tristique in non amet ac vitae tristique, aliquam
+                vivamus a, ipsum malesuada sagittis nulla, id praesent morbi rhoncus morbi luctus. Ligula odio enim sed nisl diam urna.
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+<?php include('includes/footer.inc.php'); ?>
